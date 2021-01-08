a769059ba36821c2a41ac69381a7a665bbec76c9
이아현 <57928612+LAH1203@users.noreply.github.com>
2021-01-08 15:32:25 +0900

    [part2.chap1.section1] 클래스와 객체의 정의

---
+++ ClassTest.kt
@@ -0,0 +1,19 @@
+package chap01.section1
+
+class Car {
+    val wheel: Int = 4
+
+    fun start() {
+        println("Engine Start!")
+    }
+}
+
+class Bird
+
+fun main() {
+    val sonata: Car = Car()
+
+    println(sonata.wheel)
+    sonata.start()
+
+}

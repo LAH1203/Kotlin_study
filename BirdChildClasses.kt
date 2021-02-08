d64511ac02eaceb36813d2a64c1d54a90ab7c3a1
이아현 <57928612+LAH1203@users.noreply.github.com>
2021-02-08 16:00:08 +0900

    오버라이딩 예제 코드

--- BirdChildClasses.kt
+++ BirdChildClasses.kt
@@ -1,12 +1,15 @@
 package chap01.section3
 
 open class Bird(var name: String, var wing: Int, var beak: String) {
-    fun fly() {
+    open fun fly() {
         println("Fly")
     }
 }
 
 class Lark(name: String, wing: Int, beak: String) : Bird(name, wing, beak) {
+    override fun fly() {
+        println("Quick Fly")
+    }
     fun singHitone() {
         println("sing Hitone")
     }
@@ -17,21 +20,25 @@ class Parrot : Bird {
     constructor(name: String, wing: Int, beak: String, language: String) : super(name, wing, beak) {
         this.language = language
     }
+    override fun fly() {
+        println("Slow Fly")
+    }
     fun speak() {
         println("Speak: $language")
     }
 }
 
 fun main() {
-    val lark = Lark("mylark", 2, "short")
-    val parrot = Parrot("myparrot", 2, "long", "English")
+    val lark: Bird = Lark("mylark", 2, "short")
+    val parrot: Bird = Parrot("myparrot", 2, "long", "English")
 
     println("lark - name: ${lark.name}")
-    println("parrot - name: ${parrot.name}, lang: ${parrot.language}")
+    println("parrot - name: ${parrot.name}")
 
-    lark.singHitone()
+    // Bird 객체로 생성했음에도 오버라이딩 함수가 실행됨
+    //lark.singHitone()
     lark.fly()
 
-    parrot.speak()
+    //parrot.speak()
     parrot.fly()
 }
